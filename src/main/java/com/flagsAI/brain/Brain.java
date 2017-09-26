0d5a646e250d710dee51dc0297c1724c767dac6c
vyacheslav.khalatov <vyacheslav.khalatov@it-wk.ru>
2017-09-26 13:39:46 +0300

    Kek3

---
+++ src/main/java/com/flagsAI/brain/Brain.java
@@ -0,0 +1,14 @@
+package com.flagsAI.brain;
+
+import com.flagsAI.eye.Eye;
+import com.flagsAI.hand.Hand;
+
+public class Brain {
+    private Eye eye = Eye.getInstance();
+    private Hand hand;
+
+    public void doShit(){
+
+    }
+
+}
--- src/main/java/com/flagsAI/hand/Hand.java
+++ src/main/java/com/flagsAI/hand/Hand.java
@@ -8,6 +8,6 @@ import java.awt.*;
 public class Hand {
 
     public void click(Rectangle rect){
-        
+
     }
 }
