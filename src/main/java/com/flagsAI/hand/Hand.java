f5453b25e991b919ca9776d1664cc8cd141eb9ad
vyacheslav.khalatov <vyacheslav.khalatov@it-wk.ru>
2017-09-26 13:37:54 +0300

    Kek2

---
+++ src/main/java/com/flagsAI/hand/Hand.java
@@ -0,0 +1,13 @@
+package com.flagsAI.hand;
+
+import java.awt.*;
+
+/**
+ * Created by ganib on 23.09.2017.
+ */
+public class Hand {
+
+    public void click(Rectangle rect){
+        
+    }
+}
--- src/main/java/com/flagsAI/hand/hand.java
+++
@@ -1,8 +0,0 @@
-package com.flagsAI.hand;
-
-/**
- * Created by ganib on 23.09.2017.
- */
-public class hand {
-
-}
